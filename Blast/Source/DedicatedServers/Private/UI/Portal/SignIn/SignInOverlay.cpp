4df6725f9f2676b962921eebc7392051000fcd32
thebiggestmonstro <monstroowner@gmail.com>
2026-06-04 18:47:25 +0900

    Edit SignIn Overlay Widget Class

--- Blast/Source/DedicatedServers/Private/UI/Portal/SignIn/SignInOverlay.cpp
+++ Blast/Source/DedicatedServers/Private/UI/Portal/SignIn/SignInOverlay.cpp
@@ -5,6 +5,11 @@
 #include "UI/Portal/PortalManager.h"
 #include "UI/API/GameSessions/JoinGame.h"
 #include "Components/Button.h"
+#include "Components/WidgetSwitcher.h"
+#include "UI/Portal/SignIn/SignInPage.h"
+#include "UI/Portal/SignIn/SignUpPage.h"
+#include "UI/Portal/SignIn/ConfirmSignUpPage.h"
+#include "UI/Portal/SignIn/SuccessConfirmedPage.h"
 
 void USignInOverlay::NativeConstruct()
 {
@@ -16,6 +21,16 @@ void USignInOverlay::NativeConstruct()
 
 	PortalManager = NewObject<UPortalManager>(this, PortalManagerClass);
 	JoinGameWidget->Button_JoinGame->OnClicked.AddDynamic(this, &USignInOverlay::OnJoinGameButtonClicked);
+
+	check(Button_SignIn_Test);
+	check(Button_SignUp_Test);
+	check(Button_ConfirmSignUp_Test);
+	check(Button_SuccessConfirmed_Test);
+
+	Button_SignIn_Test->OnClicked.AddDynamic(this, &USignInOverlay::ShowSignInPage);
+	Button_SignUp_Test->OnClicked.AddDynamic(this, &USignInOverlay::USignInOverlay::ShowSignUpPage);
+	Button_ConfirmSignUp_Test->OnClicked.AddDynamic(this, &USignInOverlay::USignInOverlay::USignInOverlay::ShowConfirmSignUpPage);
+	Button_SuccessConfirmed_Test->OnClicked.AddDynamic(this, &USignInOverlay::USignInOverlay::USignInOverlay::USignInOverlay::ShowSuccessConfirmedPage);
 }
 
 void USignInOverlay::OnJoinGameButtonClicked()
@@ -39,4 +54,28 @@ void USignInOverlay::UpdateJoinGameStatusMessage(const FString& StatusMessage, b
 	{
 		JoinGameWidget->Button_JoinGame->SetIsEnabled(true);
 	}
+}
+
+void USignInOverlay::ShowSignInPage()
+{
+	check(IsValid(WidgetSwitcher) && IsValid(SignInPage));
+	WidgetSwitcher->SetActiveWidget(SignInPage);
+}
+
+void USignInOverlay::ShowSignUpPage()
+{
+	check(IsValid(WidgetSwitcher) && IsValid(SignUpPage));
+	WidgetSwitcher->SetActiveWidget(SignUpPage);
+}
+
+void USignInOverlay::ShowConfirmSignUpPage()
+{
+	check(IsValid(WidgetSwitcher) && IsValid(ConfirmSignUpPage));
+	WidgetSwitcher->SetActiveWidget(ConfirmSignUpPage);
+}
+
+void USignInOverlay::ShowSuccessConfirmedPage()
+{
+	check(IsValid(WidgetSwitcher) && IsValid(SuccessConfirmedPage));
+	WidgetSwitcher->SetActiveWidget(SuccessConfirmedPage);
 }
--- Blast/Source/DedicatedServers/Public/UI/Portal/SignIn/SignInOverlay.h
+++ Blast/Source/DedicatedServers/Public/UI/Portal/SignIn/SignInOverlay.h
@@ -8,6 +8,12 @@
 
 class UJoinGame;
 class UPortalManager;
+class UWidgetSwitcher;
+class USignInPage;
+class USignUpPage;
+class UConfirmSignUpPage;
+class USuccessConfirmedPage;
+class UButton;
 
 /**
  * 
@@ -24,10 +30,37 @@ public:
 	UPROPERTY(EditDefaultsOnly)
 	TSubclassOf<UPortalManager> PortalManagerClass;
 
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UWidgetSwitcher> WidgetSwitcher;
+
 protected:
 	virtual void NativeConstruct() override;
 
 private:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<USignInPage> SignInPage;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<USignInPage> SignUpPage;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<USignInPage> ConfirmSignUpPage;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<USignInPage> SuccessConfirmedPage;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_SignIn_Test;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_SignUp_Test;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_ConfirmSignUp_Test;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_SuccessConfirmed_Test;
+
 	UPROPERTY()
 	TObjectPtr<UPortalManager> PortalManager;
 
@@ -36,4 +69,16 @@ private:
 
 	UFUNCTION()
 	void UpdateJoinGameStatusMessage(const FString& StatusMessage, bool bResetJoinGameButton);
+
+	UFUNCTION()
+	void ShowSignInPage();
+
+	UFUNCTION()
+	void ShowSignUpPage();
+
+	UFUNCTION()
+	void ShowConfirmSignUpPage();
+
+	UFUNCTION()
+	void ShowSuccessConfirmedPage();
 };
