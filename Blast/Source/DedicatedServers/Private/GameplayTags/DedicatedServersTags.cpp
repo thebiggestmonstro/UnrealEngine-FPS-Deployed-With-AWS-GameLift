e535bcf53ac1976dfa6eecfadd0fd2ca863a11df
thebiggestmonstro <monstroowner@gmail.com>
2026-06-07 18:43:55 +0900

    Add Portal Gateway and Connect with DA

--- Blast/Source/DedicatedServers/Private/GameplayTags/DedicatedServersTags.cpp
+++ Blast/Source/DedicatedServers/Private/GameplayTags/DedicatedServersTags.cpp
@@ -11,4 +11,11 @@ namespace DedicatedServersTags
 		UE_DEFINE_GAMEPLAY_TAG_COMMENT(FindOrCreateGameSession, "DedicatedServersTags.GameSessionsAPI.FindOrCreateGameSession", "Retrieves an ACTIVE game session, creating one if one doesn't exist on the GameSessions API")
 		UE_DEFINE_GAMEPLAY_TAG_COMMENT(CreatePlayerSession, "DedicatedServersTags.GameSessionsAPI.CreatePlayerSession", "Creates a new Player Session on the GameSessions API")
 	}
+
+	namespace PortalAPI
+	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(SignUp, "DedicatedServersTags.PortalAPI.SignUp", "Creates a new User in the Portal API")
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(ConfirmSignUp, "DedicatedServersTags.PortalAPI.ConfirmSignUp", "Confirms a new user in Portal API")
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(SignIn, "DedicatedServersTags.PortalAPI.SignIn", "Retrieves Access Token, Id Token, and Refresh Token in Portal API")
+	}
 }
--- Blast/Source/DedicatedServers/Public/GameplayTags/DedicatedServersTags.h
+++ Blast/Source/DedicatedServers/Public/GameplayTags/DedicatedServersTags.h
@@ -13,4 +13,11 @@ namespace DedicatedServersTags
 		UE_DECLARE_GAMEPLAY_TAG_EXTERN(FindOrCreateGameSession);
 		UE_DECLARE_GAMEPLAY_TAG_EXTERN(CreatePlayerSession);
 	}
+
+	namespace PortalAPI
+	{
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(SignUp);
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(ConfirmSignUp);
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(SignIn);
+	}
 }
