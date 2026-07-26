ac4e9b9098083c306ebf731984f01cd53a0e6fbb
thebiggestmonstro <monstroowner@gmail.com>
2026-07-26 19:16:15 +0900

    Setting for MatchStats API

--- Blast/Source/DedicatedServers/Private/GameplayTags/DedicatedServersTags.cpp
+++ Blast/Source/DedicatedServers/Private/GameplayTags/DedicatedServersTags.cpp
@@ -19,4 +19,9 @@ namespace DedicatedServersTags
 		UE_DEFINE_GAMEPLAY_TAG_COMMENT(SignIn, "DedicatedServersTags.PortalAPI.SignIn", "Retrieves Access Token, Id Token, and Refresh Token in Portal API")
 		UE_DEFINE_GAMEPLAY_TAG_COMMENT(SignOut, "DedicatedServersTags.PortalAPI.SignOut", "Signs user out and invalidates tokens in Portal API")
 	}
+
+	namespace GameStatsAPI
+	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(RecordMatchStats, "DedicatedServersTags.GameStatsAPI.RecordMatchStats", "Records the stats of a match in Game Stats API")
+	}
 }
---
+++ Blast/Source/DedicatedServers/Public/UI/GameStats/GameStatsManager.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/HTTP/HTTPRequestManager.h"
+#include "GameStatsManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEDICATEDSERVERS_API UGameStatsManager : public UHTTPRequestManager
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
